41a5f50a50b57679f7286a18589e7959873aec61
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-04-01 17:00:13 +0200

    Zad3

---
+++ Zad3/tau/src/main/java/ApacheCollections/Address.java
@@ -0,0 +1,23 @@
+package ApacheCollections;
+
+public class Address {
+    private String locality;
+    private String city;
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
---
+++ Zad3/tau/src/main/java/ApacheCollections/Customer.java
@@ -0,0 +1,37 @@
+package ApacheCollections;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private Address address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Customer(int id, String name, String locality, String city) {
+    }
+
+}
+
+
